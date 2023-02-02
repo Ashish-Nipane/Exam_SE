32fee059599f3e9bb83d129265c09092a36bc7f0
Ashish Nipane <ashishnipane09@gmail.com>
2023-02-02 15:10:32 +0530

    update primenumber.java

--- primenumber.java
+++ primenumber.java
@@ -1,5 +1,7 @@
 class primenumber{
     public static void main(String[] args){
+        System.out.println("Print prime number");
+
         int num =20;
         int count;
         for(int i =1;i<=num;i++){
